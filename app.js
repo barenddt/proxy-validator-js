32ad948c45759b8e86f2e84a51929a6ec12a0f60
Barend Du Toit <im@barend.io>
2018-10-18 12:48:46 -0600

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const proxyChecker = require("./index");
-
-proxyChecker({
-  input: "proxies.txt",
-  output: "newProxies.txt",
-  url: "http://soundcloud.com/",
-  threads: 400,
-  timeout: 10000
-});
